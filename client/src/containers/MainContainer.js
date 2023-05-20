3bb45e3c14cf89a5ef62a16581ec92e9046129ee
ewangomolka <ewangomolka@gmail.com>
2023-05-20 16:24:22 +0100

    main container updated

--- client/src/containers/MainContainer.js
+++ client/src/containers/MainContainer.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import NavBar from '../components/MainComponents.js/Navbar';
 import FeedList from '../components/MainComponents.js/FeedList';
+import { getMemes } from '../services/MainServices';
 
 const MainContainer = () => {
+
+    const [feedItems, setFeedItems] = useState([]);
+
+    useEffect(() => {
+        getMemes().then((allMemes) => {
+            setFeedItems(allMemes)
+        })
+    }, [feedItems])
+
     return ( 
         <div>
             <h1>Your Feed!</h1>
@@ -11,7 +21,7 @@ const MainContainer = () => {
             <NavBar/>
             </div>
             <div>
-            <FeedList/>
+            <FeedList feedItems={feedItems}/>
             </div>
             <footer>H.E.C. ltd</footer>
         </div>
