bc9f76dbfc452e92825aa0e011504d2fee8cb680
ewangomolka <ewangomolka@gmail.com>
2023-05-20 11:37:06 +0100

    profile component

---
+++ client/src/components/UserComponents.js/Profile.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import NavBar from '../components/MainComponents.js/NavBar';
+
+const Profile = ({users}) => {
+
+    const userPostList = users.post.map((user) => {
+        <FeedItem
+        key={user._id}
+        />
+    })
+
+    return ( 
+        <div>
+            <div>
+                <NavBar/>
+            </div>
+            <div>
+                <h2>{user.name}</h2>
+                <p>Your posts:</p>
+                <div>
+                    {userPostList}
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Profile;
