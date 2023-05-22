006c0e6c5394efabfd021c03cd59788b6ef31639
ewangomolka <ewangomolka@gmail.com>
2023-05-22 11:11:28 +0100

    support

---
+++ client/src/components/MainComponents.js/Support.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Support = () => {
+    return ( 
+        <div>
+            <h1>Dont suffer in silence!</h1>
+            <p>Samaritans: 116 123</p>
+            <p>Childline: 0800 1111</p>
+            <p>Mind: 0300 123 3393</p>
+        </div>
+     );
+}
+ 
+export default Support;
