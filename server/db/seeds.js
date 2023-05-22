2fece53f4982042ec907e937abf1b3382376f1b3
Corrine <corrine.sing@gmail.com>
2023-05-22 16:25:38 +0100

    updated img links

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -12,7 +12,7 @@ db.users.insertMany([
                 comments: [
                     {
                         user: "MemeLord1",
-                        meme: "https://images.app.goo.gl/HW3VnSU96fN1sc6f9",
+                        meme: "https://i.redd.it/bptzx7ur4uj11.jpg",
                     }
                 ]
             }
@@ -28,7 +28,7 @@ db.users.insertMany([
                 comments: [
                     {
                         user: "MemeQueen5000",
-                        meme: "https://images.app.goo.gl/A4KftyDk47zBqfMB8",
+                        meme: "https://www.meme-arsenal.com/memes/f6ab39cdbfc3f344bf59a89edd22072e.jpg",
                     }
                 ]
             }
@@ -44,7 +44,7 @@ db.users.insertMany([
                 comments: [
                     {
                         user: "MemeLord1",
-                        meme: "https://images.app.goo.gl/QoLrHceVWZfnyHB47",
+                        meme: "https://media.makeameme.org/created/are-you-ok-59bfe3.jpg",
                     }
                 ]
             }
@@ -56,32 +56,32 @@ db.memes.insertMany([
     {
         name: "add css",
         category: "css",
-        url: "https://images.app.goo.gl/HW3VnSU96fN1sc6f9",
+        url: "https://i.redd.it/bptzx7ur4uj11.jpg",
     },
     {
         name: "pikachu",
         category: "css",
-        url: "https://images.app.goo.gl/cqE4bo13TbdTUCnB9",
+        url: "https://res.cloudinary.com/practicaldev/image/fetch/s--ASsKTvWD--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0j6j5aefbocnrwkobhuk.png",
     },
     {
         name: "one does not",
         category: "css",
-        url: "https://images.app.goo.gl/vVA1sHb4N19vFrL89",
+        url: "https://zendev.com/assets/img/posts/css-javascript/one-does-not-simply.jpg",
     },
     {
         name: "family guy",
         category: "css",
-        url: "https://images.app.goo.gl/LHSNSMjidUcc2c9J6",
+        url: "https://cdn.hashnode.com/res/hashnode/image/upload/v1618132016248/KgEmbHf7K.gif?w=1600&h=840&fit=crop&crop=entropy&auto=format,compress&gif-q=60&format=webm",
     },
     {
         name: "are you ok?",
         category: "ugood?",
-        url: "https://images.app.goo.gl/QoLrHceVWZfnyHB47",
+        url: "https://media.makeameme.org/created/are-you-ok-59bfe3.jpg",
     },
     {
         name: "u good baby",
         category: "ugood?",
-        url: "https://images.app.goo.gl/gdUmAwK1GEiSU6317",
+        url: "https://media.makeameme.org/created/are-you-ok-880dd2d896.jpg",
     },
     {
         name: "u good cats",
@@ -96,47 +96,47 @@ db.memes.insertMany([
     {
         name: "inbred",
         category: "cats",
-        url: "https://images.app.goo.gl/4q8LEwoQCXEVKgKx8",
+        url: "https://bestlifeonline.com/wp-content/uploads/sites/3/2018/06/cat-meme-83.jpg?quality=82&strip=all",
     },
     {
         name: "boobytrap",
         category: "cats",
-        url: "https://images.app.goo.gl/NmAiqhL4oQdopS126",
+        url: "https://s.yimg.com/ny/api/res/1.2/tQGpYDQMcvnpeBf2xDBV3w--/YXBwaWQ9aGlnaGxhbmRlcjt3PTk2MDtoPTY0MDtjZj13ZWJw/https://media.zenfs.com/en-AU/homerun/y7.beau/e0f987bfdf21cfc1a5d05d810268dce2",
     },
     {
         name: "screaming cat",
         category: "cat",
-        url: "https://images.app.goo.gl/A4KftyDk47zBqfMB8",
+        url: "https://www.meme-arsenal.com/memes/f6ab39cdbfc3f344bf59a89edd22072e.jpg",
     },
     {
         name: "staring cat",
         category: "cat",
-        url: "https://images.app.goo.gl/NLTdnVBLp7MESKZr5",
+        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkaGFHN2SMsvASJ-hIhTtFx_GNFs7ovvOFwulHe4NCmVEFPDoWnYRzGf26NnvTHcRnhT0&usqp=CAU",
     },
     {
         name: "success kid",
         category: "sucsess",
-        url: "https://images.app.goo.gl/rMR5rr6uvBYYpyyz9",
+        url: "https://media.makeameme.org/created/you-can-do-5c9199.jpg",
     },
     {
         name: "borat",
         category: "success",
-        url: "https://images.app.goo.gl/t6KjLtWLwuNCp1M1A",
+        url: "https://i.imgflip.com/5kmt1j.jpg",
     },
     {
         name: "squirrel",
         category: "success",
-        url: "https://images.app.goo.gl/ECTeBQAv1nieqQvJ8",
+        url: "https://media.makeameme.org/created/this-is-what-1es1i8.jpg",
     },
     {
         name: "gordon",
         category: "sucess",
-        url: "https://images.app.goo.gl/qPn9XAR7xjGh9toc8",
+        url: "https://i0.wp.com/winkgo.com/wp-content/uploads/2018/12/23-Great-Job-Memes-08.jpg?w=800&ssl=1",
     },
     {
         name: "success cat",
         category: "sucess",
-        url: "https://images.app.goo.gl/DwDCodmnwCPAYRBD8",
+        url: "https://www.scienceofpeople.com/wp-content/uploads/2022/07/image-54.png",
     },
     {
         name: "you look good",
@@ -186,21 +186,21 @@ db.memes.insertMany([
     {
         name: "rough day?",
         category: "here for you",
-        url: "https://images.app.goo.gl/FjWJnVLU6NrHSUqs5",
+        url: "https://images7.memedroid.com/images/UPLOADED298/61e7ae22527cd.jpeg",
     },
     {
         name: "doggos",
         category: "here for you",
-        url: "https://images.app.goo.gl/tsy4xbBx4drLWtGYA",
+        url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQyaFrCrSZlwbv0aCY5Te3rMTbaiunAgwRo4AxkeqiedfFpDOnOhCQT2ajkQuUnVLHr56Q&usqp=CAU",
     },
     {
         name: "it",
         category: "here for you",
-        url: "https://images.app.goo.gl/QEBDLrTPYstE6gY5A",
+        url: "https://www.meme-arsenal.com/memes/6c487980d4c04a70732c68658dd2e941.jpg",
     },
     {
         name: "bob",
         category: "ugood?",
-        url: "https://images.app.goo.gl/rdnwpFoQfGRcoAB7A",
+        url: "https://i.kym-cdn.com/photos/images/original/001/553/029/86d.jpg",
     }
 ])
