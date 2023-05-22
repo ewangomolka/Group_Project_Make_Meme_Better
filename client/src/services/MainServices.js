e20f1310c9b2bd3e6d644391f7216bacd8631542
Heather <heather@Kierans-MacBook.local>
2023-05-22 17:13:44 +0100

    create form

--- client/src/services/MainServices.js
+++ client/src/services/MainServices.js
@@ -13,7 +13,14 @@ export const postMeme = (payload) => {
     })
     .then(res => res.json())
 }
-
+export const createPostForUser = (payload) => {
+    return fetch(baseURL, {
+        method: 'POST',
+        body: JSON.stringify(payload),
+        headers: { 'Content-Type': 'application/json'}
+    })
+    .then(res => res.json())
+}
 export const deleteMeme = (id) => {
     return fetch(baseURL + id, {
         method: 'DELETE'
