fe3bc4469c31174e639a77effdc6812968ed4f53
ewangomolka <ewangomolka@gmail.com>
2023-05-22 10:31:48 +0100

    delete profile

--- client/src/components/UserComponents.js/Profile.js
+++ client/src/components/UserComponents.js/Profile.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import NavBar from '../components/MainComponents.js/NavBar';
 import FeedItem from '../components/MainComponents.js/FeedItem';
+import { deleteUser } from '../../services/UserServices';
 
-const Profile = ({users}) => {
+const Profile = ({users, removeUser}) => {
 
     const userPostList = users.post.map((user) => {
         <FeedItem
@@ -10,15 +11,23 @@ const Profile = ({users}) => {
         />
     })
 
+    const handleRemove = () => {
+        deleteUser(user._id)
+        .then(() => {
+            removeUser(booking._id)
+        })
+    }
+
     return ( 
         <div>
             <div>
                 <NavBar/>
             </div>
             <div>
-
+                <div>
                 <h2>{user.username}</h2>
-
+                <button onClick={handleRemove} confirm={confirm('are you sure you want to delete your profile?')}>Delete Profile</button>
+                </div>
                 <p>Your posts:</p>
                 <div>
                     {userPostList}
