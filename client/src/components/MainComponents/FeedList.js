9990bf988ed1a34d9a3a96868ca753d234aa56ae
ewangomolka <ewangomolka@gmail.com>
2023-05-23 10:24:31 +0100

    feed list updated

--- client/src/components/MainComponents/FeedList.js
+++ client/src/components/MainComponents/FeedList.js
@@ -2,17 +2,31 @@ import React, { useEffect, useState } from 'react';
 import { getUsers } from '../../services/UserServices';
 import FeedItem from './FeedItem';
 import './FeedList.css'
+import styled from 'styled-components';
 
 const FeedList = ({users, removeUser}) => {
 
-
+  const ScrollBox= styled.div`
+    height: 400px;
+    width: 400px;
+    justify-content: center;
+    overflow: auto;
+    list-style: none;
+  `
+  const Container= styled.div`
+    display: flex;
+    justify-content: center;
+    padding-top: 75px;
+  `
 
   return (
-    <div>
+    <Container>
+      <ScrollBox>
       {users.map((user) => (
         <FeedItem key={user._id} user={user} removeUser={removeUser}/>
       ))}
-    </div>
+      </ScrollBox>
+    </Container>
   );
 };
 
